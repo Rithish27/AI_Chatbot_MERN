b55b75bd694089ce09e59403c4ac733d43bac2a9
Rithish kumar <91436355+Rithish27@users.noreply.github.com>
2023-12-31 23:50:31 +0530

    Created first GET request to get All users list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,18 @@
-console.log(
-  "This is a starter kit for this amazing project."
-);
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+
+//connections & listners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(()=>{
+    app.listen(PORT,()=>
+    console.log("Server open & connected to DB")
+    );
+  })
+  .catch((err) => console.log(err));
+
+
+
+
+
